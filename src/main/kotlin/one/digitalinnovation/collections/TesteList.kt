15ab1bb48d33fc9cf2737c19f6dd820a7da2d2de
Saotomefirst <jorge.appleseed@gmail.com>
2021-10-17 16:43:08 -0300

    Operações com Listas de Objetos
    Adiconado arquivo com operações sobre Arrays de objetos, incluindo encadeamento de funções para ordenar os dados de acordo com seus atributos

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -0,0 +1,39 @@
+package one.digitalinnovation.collections
+
+fun main() {
+
+    val joao = Funcionario("João", 2000.00)
+    val pedro = Funcionario("Pedro", 1500.0)
+    val maria = Funcionario("Maria", 4000.0)
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach{
+        println(it)
+    }
+
+    println ("--------------------------")
+
+    println(funcionarios.find { it.nome == "Maria"})
+
+    println ("--------------------------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach{ println(it) }
+
+    println ("--------------------------")
+    funcionarios
+        .sortedBy { it.nome }
+        .forEach{ println(it) }
+}
+
+data class Funcionario (
+    val nome: String,
+    val salario: Double
+) {
+    override fun toString(): String =
+        """
+            Nome: $nome
+            Salário: $salario
+        """.trimIndent()
+}
